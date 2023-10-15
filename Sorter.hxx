7041136bb8cdcb646e3c27f11f1e7bf367abf3ce
mnmustafa1109 <mnmustafa1109@gmail.com>
2023-10-15 21:57:15 +0500

    commented sorter class

--- Sorter.hxx
+++ Sorter.hxx
@@ -13,32 +13,38 @@ private:
     std::vector<Passenger> passengers;
 
 public:
+    // Constructor that initializes the Sorter with a vector of Passenger objects.
     Sorter(const std::vector<Passenger> &passengerVector) : passengers(passengerVector) {}
 
+    // Sorting methods to be implemented for different sorting algorithms.
     std::vector<Passenger> quicksort(std::vector<Passenger> &passengers);
     std::vector<Passenger> selectionsort(std::vector<Passenger> &passengers);
     std::vector<Passenger> insertionsort(std::vector<Passenger> &passengers);
     std::vector<Passenger> mergesort(std::vector<Passenger> &passengers);
 
-    
+    // Method to sort and measure the time it takes using a specified sorting algorithm.
     std::vector<Passenger> sortAndMeasureTime(const std::string &sortingAlgorithm, int n)
     {
         std::vector<Passenger> sortedPassengers;
 
+        // Check for invalid input size.
         if (n <= 0)
         {
             std::cout << "Invalid number of elements specified." << std::endl;
             return sortedPassengers; // Return an empty vector
         }
 
+        // Start measuring time.
         auto start = std::chrono::high_resolution_clock::now();
 
+        // Create a copy of the input vector with the first 'n' elements.
         std::vector<Passenger> passengersCopy = {};
         for (int i = 0; i < n; i++)
         {
             passengersCopy.push_back(passengers[i]);
         }
 
+        // Choose the sorting algorithm based on the specified algorithm name.
         if (sortingAlgorithm == "quicksort")
         {
             sortedPassengers = quicksort(passengersCopy);
@@ -61,11 +67,14 @@ public:
             return sortedPassengers; // Return an empty vector
         }
 
+        // Stop measuring time.
         auto end = std::chrono::high_resolution_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
 
+        // Calculate the number of elements actually sorted.
         int elementsSorted = std::min(n, static_cast<int>(sortedPassengers.size()));
 
+        // Print sorting result and execution time.
         std::cout << "Sorted " << elementsSorted << " elements using " << sortingAlgorithm
                   << " in " << duration.count() << " microseconds." << std::endl;
 
